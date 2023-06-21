d7c9591947ca329a66ad522005ac1e388f764d4f
Shengyou Fan <shengyoufan@gmail.com>
2023-06-21 11:48:36 +0800

    desktop UI

--- desktopApp/src/jvmMain/kotlin/main.kt
+++ desktopApp/src/jvmMain/kotlin/main.kt
@@ -1,8 +1,79 @@
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowPosition
+import androidx.compose.ui.window.WindowState
 import androidx.compose.ui.window.application
 
 fun main() = application {
-    Window(onCloseRequest = ::exitApplication) {
-        MainView()
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "Compose for Desktop demo application",
+        state = WindowState(
+            size = DpSize(1024.dp, 768.dp),
+            position = WindowPosition.Aligned(Alignment.Center),
+        )
+    ) {
+        val username = remember { mutableStateOf(TextFieldValue()) }
+        val password = remember { mutableStateOf(TextFieldValue()) }
+
+        Row (
+            modifier = Modifier.fillMaxSize(),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Column (
+                modifier = Modifier.fillMaxHeight(),
+                verticalArrangement = Arrangement.Center,
+            ) {
+                Image(
+                    painter = painterResource(
+                        "compose-multiplatform.xml"
+                    ),
+                    contentDescription = null,
+                    modifier = Modifier.width(500.dp).height(500.dp)
+                )
+            }
+            Column(
+                modifier = Modifier.fillMaxHeight(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                Spacer(modifier = Modifier.height(20.dp))
+                TextField(
+                    label = { Text(text = "Username") },
+                    value = username.value,
+                    onValueChange = { username.value = it })
+                Spacer(modifier = Modifier.height(20.dp))
+                TextField(
+                    label = { Text(text = "Password") },
+                    value = password.value,
+                    visualTransformation = PasswordVisualTransformation(),
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+                    onValueChange = { password.value = it })
+                Spacer(modifier = Modifier.height(20.dp))
+                Button(
+                    onClick = {},
+                    shape = RoundedCornerShape(50.dp),
+                    modifier = Modifier.width(300.dp).height(50.dp)
+                ) {
+                    Text(text = "Login")
+                }
+            }
+        }
     }
 }
