7f10b8101eddd80a6f82a1817916d6216be45d75
Shengyou Fan <shengyoufan@gmail.com>
2023-06-21 22:54:42 +0800

    feat: WelcomeScreen.kt

--- shared/src/commonMain/kotlin/ui/WelcomeScreen.kt
+++ shared/src/commonMain/kotlin/ui/WelcomeScreen.kt
@@ -1,9 +1,104 @@
 package ui
 
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.MarqueeAnimationMode
+import androidx.compose.foundation.background
+import androidx.compose.foundation.basicMarquee
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.focusable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.simulateHotReload
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.BeyondBoundsLayout
+import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.semantics.Role.Companion.Image
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.LineHeightStyle
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.LayoutDirection
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.FlowPreview
+import org.jetbrains.compose.resources.ExperimentalResourceApi
+import org.jetbrains.compose.resources.painterResource
+import kotlin.math.roundToInt
 
+val purple = Color(0xFF7e52fe)
+val lightBlue = Color(0xFFbdcff6)
+
+@OptIn(ExperimentalResourceApi::class, ExperimentalFoundationApi::class)
 @Composable
 fun WelcomeScreen() {
-    Text("Welcome")
+    Column(
+        modifier = Modifier.fillMaxSize().background(lightBlue),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        CompositionLocalProvider(LocalLayoutDirection provides LayoutDirection.Rtl) {
+            Row(
+                modifier = Modifier.fillMaxWidth().wrapContentHeight()
+                    .basicMarquee(
+                        animationMode = MarqueeAnimationMode.Immediately,
+                        velocity = 100.dp
+                    )
+            ) {
+                (0..10).forEach { _ ->
+                    Image(
+                        modifier = Modifier.size((100..200).shuffled().first().dp)
+                            .padding(
+                                start = (10..30).shuffled().first().dp,
+                                top = (0..60).shuffled().first().dp
+                            ),
+                        painter = painterResource("icon_cloud.xml"),
+                        contentDescription = null
+                    )
+                }
+            }
+        }
+        Image(
+            modifier = Modifier.size(300.dp),
+            painter = painterResource("icon_kodee.xml"),
+            contentDescription = null
+        )
+
+        Text(
+            textAlign = TextAlign.Center,
+            fontSize = 44.sp,
+            fontWeight = FontWeight.Bold,
+            text = "Welcome,\nLet's Kotlin!",
+            color = Color.Black,
+        )
+
+    }
+
 }
+
+
