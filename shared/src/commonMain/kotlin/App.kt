29f8ae1b5831658c54473ce74382481de6ce3ae4
TinaL <tina.tsaiting@gmail.com>
2023-06-24 21:50:43 +0800

    feat: page change animation

--- shared/src/commonMain/kotlin/App.kt
+++ shared/src/commonMain/kotlin/App.kt
@@ -1,10 +1,29 @@
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.ContentTransform
+import androidx.compose.animation.Crossfade
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.SizeTransform
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutHorizontally
+import androidx.compose.animation.slideOutVertically
+import androidx.compose.animation.with
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import client.AuthClient
 import data.LoginRequest
+import data.LoginResponse
 import kotlinx.coroutines.launch
 import ui.LoginScreen
 import ui.Screen
@@ -13,26 +32,38 @@ import viewmodel.AppViewModel
 
 expect fun getPlatformName(): String
 
+@OptIn(ExperimentalAnimationApi::class)
 @Composable
 fun App() {
-    val screenState: MutableState<Screen> = remember { mutableStateOf(Screen.LOGIN) }
+    var screenState by remember { mutableStateOf(Screen.LOGIN) }
     val appViewModel = AppViewModel()
     val client = AuthClient()
 
     MaterialTheme {
-        when (screenState.value) {
-            Screen.LOGIN -> LoginScreen(onLogin = { username, password ->
-                appViewModel.viewModelScope.launch {
-                    val response = client.login(
-                        LoginRequest(username, password)
-                    )
-
-                    if (response.result) {
-                        screenState.value = Screen.WELCOME
+        AnimatedContent(
+            targetState = screenState,
+            transitionSpec = {
+                (slideInHorizontally { it } + fadeIn(animationSpec = tween(durationMillis = 2000)) with
+                        slideOutHorizontally { -it })
+                    .using(
+                    SizeTransform(clip = false)
+                )
+            }
+        ) { screen ->
+            when (screen) {
+                Screen.LOGIN -> LoginScreen(onLogin = { username, password ->
+                    appViewModel.viewModelScope.launch {
+                        val response = client.login(
+                            LoginRequest(username, password)
+                        )
+                        if (response.result) {
+                            screenState = Screen.WELCOME
+                        }
                     }
-                }
-            })
-            Screen.WELCOME -> WelcomeScreen()
+                })
+
+                Screen.WELCOME -> WelcomeScreen()
+            }
         }
     }
 }
--- shared/src/commonMain/kotlin/ui/WelcomeScreen.kt
+++ shared/src/commonMain/kotlin/ui/WelcomeScreen.kt
@@ -1,101 +1,85 @@
 package ui
 
-import androidx.compose.animation.core.Animatable
-import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.spring
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.expandVertically
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.slideInVertically
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.MarqueeAnimationMode
 import androidx.compose.foundation.background
-import androidx.compose.foundation.basicMarquee
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.focusable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.runtime.simulateHotReload
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.FocusRequester
-import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.BeyondBoundsLayout
-import androidx.compose.ui.platform.LocalLayoutDirection
-import androidx.compose.ui.semantics.Role.Companion.Image
-import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.IntOffset
-import androidx.compose.ui.unit.LayoutDirection
-import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import kotlinx.coroutines.FlowPreview
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 import org.jetbrains.compose.resources.painterResource
-import kotlin.math.roundToInt
 
-val purple = Color(0xFF7e52fe)
+
 val lightBlue = Color(0xFFbdcff6)
 
 @OptIn(ExperimentalResourceApi::class, ExperimentalFoundationApi::class)
 @Composable
 fun WelcomeScreen() {
+    val infiniteTransition = rememberInfiniteTransition()
     Column(
         modifier = Modifier.fillMaxSize().background(lightBlue),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        CompositionLocalProvider(LocalLayoutDirection provides LayoutDirection.Rtl) {
-            Row(
-                modifier = Modifier.fillMaxWidth().wrapContentHeight()
-                    .basicMarquee(
-                        animationMode = MarqueeAnimationMode.Immediately,
-                        velocity = 100.dp
-                    )
-            ) {
-                (0..10).forEach { _ ->
-                    Image(
-                        modifier = Modifier.size((100..200).shuffled().first().dp)
-                            .padding(
-                                start = (10..30).shuffled().first().dp,
-                                top = (0..60).shuffled().first().dp
-                            ),
-                        painter = painterResource("icon_cloud.xml"),
-                        contentDescription = null
-                    )
-                }
-            }
-        }
+
+        val position by infiniteTransition.animateFloat(
+            initialValue = -60f, targetValue = 60f, animationSpec = infiniteRepeatable(
+                animation = tween(
+                    800, easing = FastOutSlowInEasing
+                ), repeatMode = RepeatMode.Reverse
+            )
+        )
+        val density = LocalDensity.current
+
         Image(
-            modifier = Modifier.size(300.dp),
+            modifier = Modifier.size(300.dp).offset(x = position.dp),
             painter = painterResource("icon_kodee.xml"),
             contentDescription = null
         )
 
-        Text(
-            textAlign = TextAlign.Center,
-            fontSize = 44.sp,
-            fontWeight = FontWeight.Bold,
-            text = "Welcome,\nLet's Kotlin!",
-            color = Color.Black,
-        )
+        AnimatedVisibility(
+            visible = true,
+            enter = slideInVertically {
+                with(density) { 200.dp.roundToPx() }
+            } +
+                    expandVertically(animationSpec = spring(stiffness = Spring.DampingRatioHighBouncy)) +
+                    fadeIn(
+                        initialAlpha = 0.3f
+                    )
+        ) {
+            Text(
+                textAlign = TextAlign.Center,
+                fontSize = 44.sp,
+                fontWeight = FontWeight.Bold,
+                text = "Welcome,\nLet's Kotlin!",
+                color = Color.Black,
+            )
+        }
 
     }
 
